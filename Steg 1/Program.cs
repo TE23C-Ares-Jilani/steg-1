c152107cb9cc014978275cbae53b41483e9f6252
Ares Jilani <ares.jilani@elev.ga.ntig>
2024-11-07 13:11:07 +0100

    re

---
+++ Steg 1/Program.cs
@@ -0,0 +1,21 @@
+﻿// if (6 > 3 || 6 == 3)
+// {
+//     Console.WriteLine("Helol world!");
+// }
+// Console.ReadLine();
+
+Console.WriteLine("Write username plz:");
+string användarnamn = Console.ReadLine();
+Console.WriteLine("Write password plz:");
+string password = Console.ReadLine();
+if (användarnamn == "kalleanka" && password == "12345")
+{
+    Console.WriteLine("Welcome!");
+    Console.ReadLine();
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("username or password wrong.");
+    Console.ReadLine();
+}
