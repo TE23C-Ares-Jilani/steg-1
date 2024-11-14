0382ebb6e13a17ba53fd393b1c81986c1c07d326
Ares Jilani <ares.jilani@elev.ga.ntig>
2024-11-14 13:15:26 +0100

    fdsfs

--- Steg 1/Program.cs
+++ Steg 1/Program.cs
@@ -28,11 +28,11 @@
 // string password = "12345";
 // string guess = "";
 // Console.WriteLine("Guess the 5 character password:");
-
+// ----------------------------------------------------------------------------------------
 // while (guess != password)
 // {
 //     guess = Console.ReadLine();
-    
+
 //     if (guess != password)
 //     {
 //         Console.WriteLine("Wrong, guess again:");
@@ -41,27 +41,74 @@
 // Console.WriteLine("Correct!");
 // Console.WriteLine("goodbye.");
 // Console.ReadKey();
-Console.WriteLine("Write a number:");
-for (int i = 0; i < 5;)
-{
-    String num = Console.ReadLine();
-    int number = 0;
-    bool success = int.TryParse(num, out number);
-    
-    if (success == false)
-    {
-        Console.WriteLine("plz write a number");
-    }
-    else
-    {
-        if (number > 5)
-        {
-            Console.WriteLine("your number is higher than 5.");
-            number++;
-        }
-        else
-        {
+// ----------------------------------------------------------------------------------------------------------
+// Console.WriteLine("Write a number:");
+// for (int i = 0; i < 5;)
+// {
+//     String num = Console.ReadLine();
+//     int number = 0;
+//     bool success = int.TryParse(num, out number);
+//     if (success == false)
+//     {
+//         Console.WriteLine("plz write a number");
+//     }
+//     else
+//     {
+//         if (number > 5)
+//         {
+//             Console.WriteLine("your number is higher than 5.");
+//             i++;
+//         }
+//         else if(number == 5)
+//         {
+//             Console.WriteLine("your number is 5.");
+//             i++;
+//         }
+//         else
+//         {
+//             Console.WriteLine("your number is lower than 5.");
+//             i++;
+//         }
+//     }
+// }
+// Console.WriteLine("you have written five number, goodbye.");
+// Console.ReadLine();
+// ---------------------------------------------------
+// Console.WriteLine("write something:");
+// int i = 0;
 
-        }
+// while (i == 0)
+// {
+//     string something = Console.ReadLine();
+
+//     bool num = something.All(char.IsDigit);
+//     if (num == true)
+//     {
+//         Console.WriteLine("goodbye.");
+//         Console.ReadKey();
+//         i++;
+//     }
+//     else
+//     {
+//         Console.WriteLine("try again:");
+//     }
+// }
+// --------------------------------------------------------------------------------------
+Console.WriteLine("welcome to guess ze number!");
+int i = 0;
+while (i == 0)
+{
+    int number = 
+    Console.WriteLine("plz write your guess:");
+    string guess = Console.WriteLine();
+    int num = 0;
+    bool success = guess.All(char.IsDigit);
+    if (success = false)
+    {
+        Console.WriteLine("please write a number.");
+    }
+    else if (success = true)
+    {
+
     }
 }
